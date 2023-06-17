8c3d81828cc6a5624069aa4fe29b22c738187262
Nomadyb <Nomadaeyb@outlook.com>
2023-06-17 17:02:46 +0300

    Login , Logout

--- base/views.py
+++ base/views.py
@@ -3,8 +3,10 @@ from .models import Room , Topic
 from .forms import RoomForm 
 from django.shortcuts import redirect
 from django.db.models import Q 
-
-
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
 
@@ -17,6 +19,41 @@ from django.db.models import Q
 
 
 
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get("username") 
+        password = request.POST.get("password")
+
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,"Username does not exist")
+            # return redirect("login")
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+              login(request,user)
+              return redirect("/")
+        else:
+            messages.error(request,"Username OR password is incorrect")
+            return redirect("login")
+
+
+         
+    context = {}
+    return render(request,"base/login_register.html",context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("login")
+
+
+
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
@@ -24,8 +61,12 @@ def home(request):
         Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)  ) if q != '' else Room.objects.all()
 
     topic = Topic.objects.all()
-    context = {"rooms":rooms,"topics":topic}
+    room_count = rooms.count()
+
+    context = {"rooms":rooms,"topics":topic,"room_count":room_count,"search":q}
     return render(request,"base/home.html",context)
+
+
 
 
 def room(request,pk):
@@ -34,6 +75,7 @@ def room(request,pk):
     context = {"room":room}
     return render(request,"base/room.html",context)
 
+@login_required(login_url="login")
 def createRoom(reguest):
     form = RoomForm()
     
